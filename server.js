84adcd3d1001eb2836fe33f0672156efdddfc98a
Aditya Gupta <adityag2010@gmail.com>
2020-04-21 02:59:54 +0530

    Routes Setup
      - Add routes for users, auth, posts, profile
      - Add routes in server file

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+const authRoutes = require('./routes/api/auth');
+const postsRoutes = require('./routes/api/posts');
+const profileRoutes = require('./routes/api/profile');
+const usersRoutes = require('./routes/api/users');
+
 const PORT = process.env.PORT || 5000; // for PORT on server environment
 
 const app = express();
@@ -9,6 +14,12 @@ const app = express();
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running!'));
+
+// Define Routes
+app.use('/api/users', usersRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/profile', profileRoutes);
+app.use('/api/posts', postsRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}!`);
